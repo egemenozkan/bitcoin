57b0ecda7589fe2472675652ab29f976e93d9680
egemenozkan@gmail.com <egemenozkan@gmail.com>
2019-11-08 21:59:22 +0300

    first commit

---
+++ src/views/Projects.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+     Projects
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'projects',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
